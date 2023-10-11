cab2a4c1ce61f37273f8d3e33fc4bd5d468d0d8b
LMR <dlaudfuf33@gmail.com>
2023-10-11 14:18:56 +0900

    1011

---
+++ my_calculator.py
@@ -0,0 +1,15 @@
+# calculator.py
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Division by zero is not allowed"
+    return x / y
